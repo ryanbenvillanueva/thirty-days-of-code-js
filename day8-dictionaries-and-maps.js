a65ecc79feeb0ebbb82dd6d8d406bb55e9e3e9a5
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-04-28 18:54:06 +0800

    Solve Day 8: Dictionaries and Maps

---
+++ day8-dictionaries-and-maps.js
@@ -0,0 +1,37 @@
+function processData(input) {
+  const arr = input.split('\n');
+  let phonebook = [];
+  let queries = [];
+  arr.forEach(str => {
+    if (str === arr[0]) {
+      return;
+    } else if (str.includes(" ")) {
+      let parts = str.split(" ");
+
+      phonebook.push(parts);
+    } else {
+      queries.push(str);
+    }
+  });
+  
+  let myPhonebook = new Map(phonebook);
+
+  queries.forEach(query => {
+    if (myPhonebook.has(query)) {
+      console.log(query + "=" + myPhonebook.get(query));
+    } else {
+      console.log('Not found');
+    }
+  });
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
